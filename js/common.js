661ede717631b0377a5ada44cb22754eb8ffca9b
Dheeraj Kumar Sidana <dk@reelearn.ai>
2022-12-02 12:47:22 +0530

    fixing merge conflict

--- js/common.js
+++ js/common.js
@@ -5,27 +5,14 @@ $(function () {
     $('.dwr-close').on('click', function () {
         $(this).closest('.drawer').removeClass('open');
     });
-<<<<<<< HEAD
 
     var secName;
     // For Desktop
     $('.hd-nav-desktop-list .section-nav-link').on('click', function () {
-=======
-    // $('.get-started-btn').on('click', function () {
-    //     $('html,body').animate({
-    //         scrollTop: $(".hp-form-section").offset().top
-    //     },
-    //         'slow');
-    // });
-
-    var secName;
-    $('.hd-nav-desktop-listitem-link').on('click', function () {
->>>>>>> 2dbc7dc11d60eb500408e4c2467ca52763b54ba9
         secName = $(this).attr('data-section');
         $('html,body').animate({
             scrollTop: $("#"+ secName).offset().top
         },'slow');
-<<<<<<< HEAD
     });
 
     // For Mobile
@@ -37,7 +24,5 @@ $(function () {
         $('html,body').animate({
             scrollTop: $("#"+ secName).offset().top
         },'slow');
-=======
->>>>>>> 2dbc7dc11d60eb500408e4c2467ca52763b54ba9
     });
 });
